35dc07eb4bde46b1a7ca75a7d51589b3f9719588
anmol <me@aghadia.com>
2024-08-19 20:22:42 -0700

    added hasUnique

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Sudoku-game
 Sudoku generator and solver 
+
+## Generation logic
+1) `STEP A`Fill a board with random numbers (1-9) while checking for validity
+
+1) `STEP B`
+    1) `STEP 1` make an empty array `visited`
+    1) `STEP 2` Remove a random cell `C` that is not in `visited`
+    1) `STEP 3` Check if unique solution exists
+    1) `STEP 4`
+        - if unique exists:
+            - if target number of cells achieved, go to `STEP 5` after removing cell `C`
+            - Remove the cell `C` and loop back to `STEP 2`
+        - else, go back to `STEP 2` and keep track of cell `C` in array `visited` without removing cell
+
+
+### STEP 1:
+1) start with empty board filled with 0's
+1) Do the following for all cells in order
+    1) make an array with range[1,9]
+    1) remove all numbers that:
+        1) are present in its row
+        1) are present in its column
+        1) are present in its box
+    1) the remaining array is `arr`
+    1) randomly pick a number from `arr` for the cell
+
+### STEP 2:
+1) Pick two random numbers `i`, `j` in the range[1,9]
+1) mark this cell as empty (0)
+
+### STEP 3:
+1) Loop over all cells in order
+    1) 
+
+## Dificulty params
+should have exactly
+1) Easy: 42 [36-49]
+1) Medium: 34 [32-35]
+1) Hard: 30 [28-31]
+1) Very Hard: 26 [24-27]
+1) `test and keep if possible !!!` Evil: 17
--- solve.ts
+++ solve.ts
@@ -1,17 +1,29 @@
-// Board is a 2D list of integers
-// let board1: number[][] = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0]
-// ];
+let boardEmpty: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
-let board: number[][] = [
+let boardCompleteExceptOne: number[][] = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 0]
+];
+
+
+let exampleQuestionBoard: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 1, 2],
     [0, 0, 0, 0, 0, 0, 0, 3, 4],
     [0, 0, 0, 0, 0, 0, 0, 5, 6],
@@ -22,6 +34,64 @@ let board: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 6, 7],
     [0, 0, 0, 0, 0, 0, 0, 8, 9]
 ];
+
+let exampleQuestionBoardSolved: number[][] = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+// Returns true if the incomplete board exactly a single solution
+function hasUnique(board: number[][]): boolean {
+    if (isSolved(board)) return true;
+
+    // returns the number of solutions
+    function hasUniqueHelper(board: number[][]): number {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (board[row][col] === 0) { // Check if empty cell exists
+                    let numberOfSolutions = 0;
+                    for (let num = 1; num <= 9; num++) { // then fill
+                        if (isValid(board, row, col, num)) { // ignore
+                            board[row][col] = num;
+                            if (isSolved(board)) { // returns true if board is solved
+                                numberOfSolutions++;
+                            } else {
+                                numberOfSolutions += hasUniqueHelper(board);
+                                board[row][col] = 0; // some cells are empty
+                            }
+                        }
+                    }
+                    return numberOfSolutions;
+                }
+            }
+        }
+        return -1;
+    }
+
+    return hasUniqueHelper(board) == 1;
+}
+
+// returns true if the board is already solved
+function isSolved(board: number[][]): boolean {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] === 0) {
+                return false;   
+            }
+            if (!isValid(board, row, col, board[row][col])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 // Solves the board recursively using backtracking
 function solveBoard(board: number[][]): boolean {
@@ -44,36 +114,15 @@ function solveBoard(board: number[][]): boolean {
     return true;
 }
 
-// function isUnique(board: number[][], count: number): boolean {
-//     // let count = 0;
-//     for (let row = 0; row < 9; row++) {
-//         for (let col = 0; col < 9; col++) {
-//             if (board[row][col] === 0) {
-//                 for (let num = 1; num <= 9; num++) {
-//                     if (isValid(board, row, col, num)) {
-//                         board[row][col] = num;
-//                         count++;
-//                         if (isUnique(board, count)) {
-//                             return true;
-//                         }
-//                         board[row][col] = 0;
-//                     }
-//                 }
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
-
+// Returns true if the num is allowed in the give row and col of given board
 function isValid(board: number[][], row: number, col: number, num: number): boolean {
-    return !inRow(board, row, num) && !inCol(board, col, num) && !inBox(board, row, col, num);
+    return !inRow(board, row, col, num) && !inCol(board, row, col, num) && !inBox(board, row, col, num);
 }
 
 // Checks if a number is valid in a given row
-function inRow(board: number[][], row: number, num: number): boolean {
+function inRow(board: number[][], row: number, col: number, num: number): boolean {
     for (let i = 0; i < 9; i++) {
-        if (board[row][i] === num) {
+        if (board[row][i] === num && (i !== col)) {
             return true;
         }
     }
@@ -81,9 +130,9 @@ function inRow(board: number[][], row: number, num: number): boolean {
 }
 
 // Checks if a number is valid in a given column
-function inCol(board: number[][], col: number, num: number): boolean {
+function inCol(board: number[][], row: number, col: number, num: number): boolean {
     for (let i = 0; i < 9; i++) {
-        if (board[i][col] === num) {
+        if (board[i][col] === num && (i !== row)) {
             return true;
         }
     }
@@ -96,12 +145,10 @@ function inBox(board: number[][], row: number, col: number, num: number): boolea
     let start_col = Math.floor(col / 3) * 3;
     for (let i = start_row; i < start_row + 3; i++) {
         for (let j = start_col; j < start_col + 3; j++) {
-            if (board[i][j] === num) {
+            if (board[i][j] === num && (row !== i) && (col !== j)) {
                 return true;
             }
         }
     }
     return false;
 }
-solveBoard(board);
-console.log(board2);
